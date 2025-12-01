84acd6c86b2b002ddca01667f9a9401f4b1ca8e5
Alwin Zhao <alwinzhao12@gmail.com>
2025-12-01 11:15:06 -0500

    include package statement in App.js

--- src/main/java/com/pluralsight/App.java
+++ src/main/java/com/pluralsight/App.java
@@ -1,6 +1,6 @@
+package com.pluralsight;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import com.pluralsight.*;
 
 public class App {
     final static Logger logger = LogManager.getLogger(App.class);
